9e1b4ace29a29157667268d78b1df81d6468a7c9
LAPTOP-JS7PJA3E\Collin <Collin.d.reed@gmail.com>
2021-05-15 21:16:20 -0400

    basic recipe cards

--- recipeShoppingList/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipeShoppingList/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a test', 'https://img.icons8.com/cotton/2x/circled-down--v2.png'),
+    new Recipe('A SECOND recipe', 'just filling description space', 'https://cdn.pixabay.com/photo/2015/06/05/10/35/dishes-798316_1280.png')
+  ];
 
   constructor() { }
 
