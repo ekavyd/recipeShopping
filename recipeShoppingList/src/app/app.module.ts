a6aa0ab6592887401f2db8877f12d729d6918324
LAPTOP-JS7PJA3E\Collin <Collin.d.reed@gmail.com>
2021-05-27 16:11:48 -0400

    add custom directive to expand bootstrap butotn on click

--- recipeShoppingList/src/app/app.module.ts
+++ recipeShoppingList/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { RecipesComponent } from './recipes/recipes.component';
     RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    RecipesComponent
+    RecipesComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
